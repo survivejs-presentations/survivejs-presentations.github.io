81620e9d229ba415c7e17cf3ea167fe82fb26e9c
Juho Vepsalainen <bebraw@gmail.com>
2017-09-07 10:19:35 +0300

    chore: Apply prettier and add a link to a new presentation

--- presentation/index.js
+++ presentation/index.js
@@ -39,9 +39,12 @@ require("spectacle/lib/themes/default/index.css");
 require("./custom.css");
 
 const slideTransition = ["slide"];
-const images = mapValues({
-  survivejs: require("../images/survivejs.png")
-}, v => v.replace('/', ''));
+const images = mapValues(
+  {
+    survivejs: require("../images/survivejs.png")
+  },
+  v => v.replace("/", "")
+);
 
 preloader(images);
 
@@ -64,78 +67,185 @@ export default class Presentation extends React.Component {
           </Slide>
 
           <Slide transition={slideTransition}>
-            <Heading size={1}>
-              React Presentations
-            </Heading>
+            <Heading size={1}>React Presentations</Heading>
             <List>
-              <ListItem><Link href="https://presentations.survivejs.com/react-components/">React Components</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/react-and-redux/">React and Redux</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/react-architecture/">React Architecture</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/forms-in-react/">Forms in React</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/react-performance/">React Performance</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/react-api-design/">React API Design - Case Reactabular</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/react-compatible-alternatives/">React Compatible Alternatives</Link></ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-components/">
+                  React Components
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-and-redux/">
+                  React and Redux
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-architecture/">
+                  React Architecture
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/forms-in-react/">
+                  Forms in React
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-performance/">
+                  React Performance
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-api-design/">
+                  React API Design - Case Reactabular
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-compatible-alternatives/">
+                  React Compatible Alternatives
+                </Link>
+              </ListItem>
             </List>
           </Slide>
 
           <Slide transition={slideTransition}>
-            <Heading size={1}>
-              Webpack Presentations
-            </Heading>
+            <Heading size={1}>Webpack Presentations</Heading>
             <List>
-              <ListItem><Link href="https://presentations.survivejs.com/re-imagining-webpack/">Re-imagining Webpack</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/webpack-from-apprentice-to-journeyman/">Webpack - From Apprentice to Journeyman</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/webpack-from-journeyman-to-master/">Webpack - From Journeyman to Master</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/webpack-the-good-parts/">Webpack - The Good Parts</Link></ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/re-imagining-webpack/">
+                  Re-imagining Webpack
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/webpack-from-apprentice-to-journeyman/">
+                  Webpack - From Apprentice to Journeyman
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/webpack-from-journeyman-to-master/">
+                  Webpack - From Journeyman to Master
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/webpack-the-good-parts/">
+                  Webpack - The Good Parts
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/webpack-the-react-parts/">
+                  Webpack - The React Parts
+                </Link>
+              </ListItem>
             </List>
           </Slide>
 
           <Slide transition={slideTransition}>
-            <Heading size={1}>
-              Other Presentations
-            </Heading>
+            <Heading size={1}>Other Presentations</Heading>
             <List>
-              <ListItem><Link href="https://presentations.survivejs.com/javascript-in-2016/">JavaScript in 2016</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/how-to-write-a-book-and-survivejs-it/">How to write a book and SurviveJS it</Link></ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/javascript-in-2016/">
+                  JavaScript in 2016
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/how-to-write-a-book-and-survivejs-it/">
+                  How to write a book and SurviveJS it
+                </Link>
+              </ListItem>
             </List>
           </Slide>
 
           <Slide transition={slideTransition}>
-            <Heading size={1}>
-              Legacy Presentations
-            </Heading>
+            <Heading size={1}>Legacy Presentations</Heading>
             <List>
-              <ListItem><Link href="https://presentations.survivejs.com/javascript-quo-vadis/">JavaScript - Quo Vadis?</Link> - A look at the state of JavaScript ecosystem from early 2016</ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/frontend-in-2015/">Frontend in 2015</Link> - A look at the state of frontend in 2015</ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/training/">SurviveJS React Training</Link> - Split up to smaller presentations earlier. Still interesting, though</ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/react-in-2016/">React in 2016</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/state-management-in-react/">State Management in React</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/structuring-react-projects/">Structuring React Projects</Link></ListItem>
-              <ListItem><Link href="https://presentations.survivejs.com/advanced-webpack/">Advanced Webpack</Link></ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/javascript-quo-vadis/">
+                  JavaScript - Quo Vadis?
+                </Link>{" "}
+                - A look at the state of JavaScript ecosystem from early 2016
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/frontend-in-2015/">
+                  Frontend in 2015
+                </Link>{" "}
+                - A look at the state of frontend in 2015
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/training/">
+                  SurviveJS React Training
+                </Link>{" "}
+                - Split up to smaller presentations earlier. Still interesting,
+                though
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/react-in-2016/">
+                  React in 2016
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/state-management-in-react/">
+                  State Management in React
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/structuring-react-projects/">
+                  Structuring React Projects
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://presentations.survivejs.com/advanced-webpack/">
+                  Advanced Webpack
+                </Link>
+              </ListItem>
             </List>
           </Slide>
 
           <Slide transition={slideTransition}>
-            <Heading size={1}>
-              About Author
-            </Heading>
+            <Heading size={1}>About Author</Heading>
             <List>
-              <Appear><ListItem>Juho Vepsäläinen</ListItem></Appear>
-              <Appear><ListItem>Subcontractor gone author (est. 2015)</ListItem></Appear>
-              <Appear><ListItem>Thousands of books sold, growing community</ListItem></Appear>
-              <Appear><ListItem>Academic background (C, Python, JavaScript)</ListItem></Appear>
-              <Appear><ListItem><Link href="http://www.nixtu.info/">nixtu.info</Link>, <Link href="http://survivejs.com/">SurviveJS</Link>, <Link href="http://jster.net/">JSter</Link></ListItem></Appear>
-              <Appear><ListItem><Link href="https://twitter.com/bebraw"><b>@bebraw</b></Link>, <Link href="https://twitter.com/survivejs"><b>@survivejs</b></Link>, <Link href="https://twitter.com/jsterlibs"><b>@jsterlibs</b></Link></ListItem></Appear>
+              <Appear>
+                <ListItem>Juho Vepsäläinen</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Subcontractor gone author (est. 2015)</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Thousands of books sold, growing community</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>Academic background (C, Python, JavaScript)</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>
+                  <Link href="http://www.nixtu.info/">nixtu.info</Link>,{" "}
+                  <Link href="http://survivejs.com/">SurviveJS</Link>,{" "}
+                  <Link href="http://jster.net/">JSter</Link>
+                </ListItem>
+              </Appear>
+              <Appear>
+                <ListItem>
+                  <Link href="https://twitter.com/bebraw">
+                    <b>@bebraw</b>
+                  </Link>,{" "}
+                  <Link href="https://twitter.com/survivejs">
+                    <b>@survivejs</b>
+                  </Link>,{" "}
+                  <Link href="https://twitter.com/jsterlibs">
+                    <b>@jsterlibs</b>
+                  </Link>
+                </ListItem>
+              </Appear>
             </List>
           </Slide>
 
           <Slide transition={slideTransition}>
             <Link href="http://www.survivejs.com/">
-              <Heading size={1}>
-                SurviveJS
-              </Heading>
+              <Heading size={1}>SurviveJS</Heading>
             </Link>
-            <Image src={images.survivejs} margin="0px auto 40px" height="524px"/>
+            <Image
+              src={images.survivejs}
+              margin="0px auto 40px"
+              height="524px"
+            />
           </Slide>
         </Deck>
       </Spectacle>
